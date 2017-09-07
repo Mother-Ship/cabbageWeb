658276056bc6cf96aa3f3b7581223e5032427f7c
QuHanSheng <quhansheng@gmail.com>
2017-09-07 17:50:16 +0800

    ……

--- src/main/java/top/mothership/cabbage/controller/CqController.java
+++ src/main/java/top/mothership/cabbage/controller/CqController.java
@@ -1,12 +1,20 @@
 package top.mothership.cabbage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import top.mothership.cabbage.pojo.CqRequest;
+import top.mothership.cabbage.serviceImpl.CqServiceImpl;
 
 @RequestMapping(value = "/cqAPI", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 @RestController
 public class CqController {
-
+    @Autowired
+    private CqServiceImpl cqService;
+    public void cqMsgPrase(@RequestBody CqRequest cqRequest){
+        //待整理业务逻辑
+    }
 
 }
