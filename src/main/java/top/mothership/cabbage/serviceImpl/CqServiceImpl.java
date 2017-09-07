89293afb779041778cfe010edadce1d0720dc0a0
QuHanSheng <quhansheng@gmail.com>
2017-09-07 17:50:16 +0800

    ……

--- src/main/java/top/mothership/cabbage/serviceImpl/CqServiceImpl.java
+++ src/main/java/top/mothership/cabbage/serviceImpl/CqServiceImpl.java
@@ -1,6 +1,7 @@
 package top.mothership.cabbage.serviceImpl;
 
+import org.springframework.stereotype.Service;
 import top.mothership.cabbage.service.CqService;
-
+@Service
 public class CqServiceImpl implements CqService {
 }
