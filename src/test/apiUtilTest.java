11a23ff8757eacb2271fcb94af66c7acf8b94156
QuHanSheng <quhansheng@gmail.com>
2017-09-12 17:54:19 +0800

    待解决欢迎新人的bug

--- src/test/apiUtilTest.java
+++ src/test/apiUtilTest.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import org.jsoup.Connection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,9 +7,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import top.mothership.cabbage.mapper.BaseMapper;
 import top.mothership.cabbage.pojo.Beatmap;
+import top.mothership.cabbage.pojo.CqMsg;
 import top.mothership.cabbage.pojo.Score;
 import top.mothership.cabbage.pojo.Userinfo;
 import top.mothership.cabbage.util.ApiUtil;
+import top.mothership.cabbage.util.CqUtil;
 import top.mothership.cabbage.util.ImgUtil;
 
 import java.sql.Date;
@@ -25,10 +28,21 @@ public class apiUtilTest {
     @Autowired
     private ImgUtil imgUtil;
     @Autowired
+    private CqUtil cqUtil;
+    @Autowired
     private BaseMapper baseMapper;
 
     @Test
     public void Test() {
+
+        CqMsg cqMsg = new CqMsg();
+        cqMsg.setPostType("event");
+        cqMsg.setMessageType("group");
+        cqMsg.setGroupId(532783765L);
+        cqMsg.setUserId(1335734657L);
+        cqMsg.setMessage("[CQ:at,qq="+cqMsg.getUserId()+"]，欢迎加入本群。");
+        cqUtil.sendMsg(cqMsg);
+
 //        Userinfo u1 = baseMapper.getNearestUserInfo(Date.valueOf("2017-9-11"), 2545898);
 //        Userinfo u = apiUtil.getUser("Mother Ship", null);
 //        imgUtil.drawUserInfo(u, u1, "creep", 3, true, 0);
@@ -44,6 +58,7 @@ public class apiUtilTest {
 //            map.put(list.get(i), i);
 //        }
 //        imgUtil.drawUserBP("Mother Ship",map);
+
 
     }
 
