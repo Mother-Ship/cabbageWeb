39ee694402bb0921f79f5a4ec2c3503c06141667
瞿瀚盛 <quhansheng@gmail.com>
2017-09-13 08:40:05 +0800

    修复了欢迎新人……之前忘记了编码

--- src/main/java/top/mothership/cabbage/util/MsgUtil.java
+++ src/main/java/top/mothership/cabbage/util/MsgUtil.java
@@ -10,17 +10,14 @@ import top.mothership.cabbage.pojo.CqMsg;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class MsgUtil {
-    private static String cmdRegex = "[!！]([^ ]+)([^]*)";
+    private static String adminCmdRegex = "[!！]sudo ([^ ]*)(.*)";
+    private static String cmdRegex = "[!！]([^ ]+)(.*)";
     private static String cmdRegexWithNum = "[!！]([^ ]+)([^#]*) #(.+)";
-    private final ApiUtil apiUtil;
-    private final CqUtil cqUtil;
-    private final ImgUtil imgUtil;
-    private final WebPageUtil webPageUtil;
-    private BaseMapper baseMapper;
-    private Logger logger = LogManager.getLogger(this.getClass());
     private static String singleImgRegex = "\\[CQ:image,file=(.+)\\]";
     private static String[] msgs = new String[100];
     private static int start = 0;
@@ -28,9 +25,15 @@ public class MsgUtil {
     private static int len = 0;
     private static List<Long> mp5Admin = Arrays.asList(2643555740L, 290514894L, 2307282906L, 2055805091L, 735862173L,
             1142592265L, 263202941L, 992931505L, 1335734657L, 526942417L, 1012621328L);
-    private static List<Long> mp4Admin = Arrays.asList(89217167L,295539897L,290514894L,2307282906L,
-            2643555740L,2055805091L,954176984L,879804833L,526942417L);
+    private static List<Long> mp4Admin = Arrays.asList(89217167L, 295539897L, 290514894L, 2307282906L,
+            2643555740L, 2055805091L, 954176984L, 879804833L, 526942417L);
     private static List<CqMsg> inviteRequests = new ArrayList<>();
+    private final ApiUtil apiUtil;
+    private final CqUtil cqUtil;
+    private final ImgUtil imgUtil;
+    private final WebPageUtil webPageUtil;
+    private BaseMapper baseMapper;
+    private Logger logger = LogManager.getLogger(this.getClass());
 
     @Autowired
     public MsgUtil(ApiUtil apiUtil, CqUtil cqUtil, ImgUtil imgUtil, WebPageUtil webPageUtil, BaseMapper baseMapper) {
@@ -41,27 +44,74 @@ public class MsgUtil {
         this.baseMapper = baseMapper;
     }
 
-    public void AdminCmd(CqMsg cqMsg){
+    public void AdminCmd(CqMsg cqMsg) {
+        String msg = cqMsg.getMessage();
+        Matcher m = Pattern.compile(cmdRegex).matcher(msg);
+        m.find();
+        switch (m.group(1)) {
+            case "stat":
+                break;
+            case "bp":
+                break;
+            case "setid":
+                break;
+            case "statme":
+                break;
+            case "bpme":
+                break;
+            case "recent":
+                break;
+            case "help":
+                break;
+        }
 
     }
-    public void CommonCmd(CqMsg cqMsg){
+
+    public void CommonCmd(CqMsg cqMsg) {
+        String msg = cqMsg.getMessage();
+        Matcher m = Pattern.compile(adminCmdRegex).matcher(msg);
+        m.find();
+        switch (m.group(1)) {
+            case "add":
+                break;
+            case "del":
+                break;
+            case "check":
+                break;
+            case "褪裙": case "退群":
+                break;
+            case "bg":
+                break;
+            case "recent":
+                break;
+            case "afk":
+                break;
+            case "smoke":
+                break;
+            case "invite":
+                break;
+
+        }
+
 
     }
-    public void stashInviteRequest(CqMsg cqMsg){
+
+    public void stashInviteRequest(CqMsg cqMsg) {
         //等待日后调用
         inviteRequests.add(cqMsg);
     }
-    public void Smoke(CqMsg cqMsg){
+
+    public void Smoke(CqMsg cqMsg) {
         //这里拿到的是没有刮去图片的
         int count = 0;
         String msg = cqMsg.getMessage();
-        if(msg.matches(singleImgRegex)){
+        if (msg.matches(singleImgRegex)) {
             msg = "Image";
         }
         //刮掉除了中文英文数字之外的东西
         msg = msg.replaceAll("[^\\u4e00-\\u9fa5a-zA-Z0-9]", "");
         //循环数组
-        if (cqMsg.getGroupId()==201872650 || cqMsg.getGroupId()==564679329 ||cqMsg.getGroupId()==532783765) {
+        if (cqMsg.getGroupId() == 201872650 || cqMsg.getGroupId() == 564679329 || cqMsg.getGroupId() == 532783765) {
             len++;
             if (len >= 100) {
                 len = 100;
@@ -79,18 +129,18 @@ public class MsgUtil {
             if (start < end) {
                 //复读不抓三个字以下的和纯图片
                 for (int i = 0; i < end; i++) {
-                    if ((cqMsg.getGroupId() + msg).equals(msgs[i])&&!msg.equals("Image") && msg.length() >= 3) {
+                    if ((cqMsg.getGroupId() + msg).equals(msgs[i]) && !msg.equals("Image") && msg.length() >= 3) {
                         count++;
                     }
                 }
             } else {
                 for (int i = 0; i < start - 1; i++) {
-                    if ((cqMsg.getGroupId() + msg).equals(msgs[i])&&!msg.equals("Image")  && msg.length() >= 3) {
+                    if ((cqMsg.getGroupId() + msg).equals(msgs[i]) && !msg.equals("Image") && msg.length() >= 3) {
                         count++;
                     }
                 }
                 for (int i = end; i < msgs.length; i++) {
-                    if ((cqMsg.getGroupId() + msg).equals(msgs[i])&&!msg.equals("Image") && msg.length() >= 3) {
+                    if ((cqMsg.getGroupId() + msg).equals(msgs[i]) && !msg.equals("Image") && msg.length() >= 3) {
                         count++;
                     }
                 }
@@ -99,11 +149,11 @@ public class MsgUtil {
         }
         if (count >= 6) {
             String resp;
-            if (mp5Admin.contains(cqMsg.getUserId())&&cqMsg.getGroupId()==201872650) {
-                logger.info("检测到群管" + cqMsg.getUserId()+ "的复读行为");
+            if (mp5Admin.contains(cqMsg.getUserId()) && cqMsg.getGroupId() == 201872650) {
+                logger.info("检测到群管" + cqMsg.getUserId() + "的复读行为");
                 cqMsg.setMessage("[CQ:at,qq=2643555740] 检测到群管" + "[CQ:at,qq=" + cqMsg.getUserId() + "] 复读。");
 
-            } else if(mp4Admin.contains(cqMsg.getUserId())&&cqMsg.getGroupId()==564679329){
+            } else if (mp4Admin.contains(cqMsg.getUserId()) && cqMsg.getGroupId() == 564679329) {
                 logger.info("检测到群管" + cqMsg.getUserId() + "的复读行为");
                 cqMsg.setMessage("[CQ:at,qq=1012621328] 检测到群管" + "[CQ:at,qq=" + cqMsg.getUserId() + "] 复读。");
             } else {
