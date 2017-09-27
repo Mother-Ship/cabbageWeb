5e32901bc3265c10a9fc465965be717510e3fade
QuHanSheng <quhansheng@gmail.com>
2017-09-27 09:18:02 +0800

    修正时间统计，不缓存用户特定bg

--- src/main/java/top/mothership/cabbage/controller/UserController.java
+++ src/main/java/top/mothership/cabbage/controller/UserController.java
@@ -4,33 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 
-@Controller
-public class UploadController {
-    @RequestMapping(value = "/uploadFile",method = RequestMethod.POST)
-    public void upload(@RequestParam(value = "myfile", required = true) MultipartFile file, HttpServletRequest request,
-                       HttpServletResponse response)
-    {
+@RestController
+@RequestMapping(value = "/api/user")
+public class UserController {
+    @RequestMapping(value = "/upload",method = RequestMethod.POST)
+    public void upload(@RequestParam(value = "myfile", required = true) MultipartFile file) {
         String path = "C:\\CoolQ Pro\\data\\image\\resource\\db\\";
         String fileName = file.getOriginalFilename();
         System.out.println("文件上传路径为：" + path);
         File targetFile = new File(path, fileName);
-        if (!targetFile.exists())
-        {
+        if (!targetFile.exists()){
             targetFile.mkdirs();
         }
         // 保存
-        try
-        {
+        try {
             file.transferTo(targetFile);
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
