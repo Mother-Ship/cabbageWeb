a2ebc230f384f96bb40dee6b52f791b1d8654ae7
QuHanSheng <quhansheng@gmail.com>
2017-10-23 15:08:58 +0800

    修正正则表达式错误导致PNG结尾的背景无法识别的问题

--- src/main/java/top/mothership/cabbage/util/Constant.java
+++ src/main/java/top/mothership/cabbage/util/Constant.java
@@ -13,6 +13,6 @@ public class Constant {
     public final static ResourceBundle CABBAGE_CONFIG = ResourceBundle.getBundle("cabbage");
     public final static String REPEAT_FILTER_REGEX = "[^\\u4e00-\\u9fa5a-zA-Z0-9]";
     public final static String BGLINE_REGEX = "(?<=\\[Events]\\r\\n)([^\\r\\n]*)\\r\\n([^\\r\\n]*)";
-    public final static String BGNAME_REGEX = "(?<=[\\d*],[\\d*],\")(.*\\.(jpg)|(png))";
+    public final static String BGNAME_REGEX = "(?<=[\\d*],[\\d*],\")(.*\\.(jpg)|.*\\.(png))";
     public final static String DOWNLOAD_FILENAME_REGEX ="(?<=filename=\")([^\";]*)";
 }
