d4cc6101114a108c02ad7cecccc5b97b1d493f24
QHS <quhansheng@gmail.com>
2019-02-10 01:25:27 +0800

    添加新一届MP45赛群提示

--- src/main/java/top/mothership/cabbage/service/CqServiceImpl.java
+++ src/main/java/top/mothership/cabbage/service/CqServiceImpl.java
@@ -1021,6 +1021,16 @@ public class CqServiceImpl {
             case "807757470":
                 resp = "[CQ:at,qq=" + cqMsg.getUserId() + "],欢迎来到第三届MP4杯赛群。\n请修改群名片为osu! id，并且仔细阅读群公告。";
                 break;
+            case "801588216":
+                resp = "[CQ:at,qq=" + cqMsg.getUserId() + "],欢迎来到第四届MP4杯赛群。\n请修改群名片为osu! id，并且仔细阅读群公告，以及群文件中的比赛规程、给选手的建议。\n报名地址：https://goo.gl/forms/QG98HWWc5ANQtZey1 ";
+                break;
+            case "806345866":
+                resp = "[CQ:at,qq=" + cqMsg.getUserId() + "],欢迎来到第八届MP5杯赛群。\n请修改群名片为osu! id，并且仔细阅读群公告，以及群文件中的比赛规程、给选手的建议。\n报名地址：https://www.wenjuan.com/s/BbYZvi/ ";
+                break;
+                //备用
+//            case "806345866":
+//                resp = "[CQ:at,qq=" + cqMsg.getUserId() + "],欢迎来到第八届MP5杯赛群。\n请修改群名片为osu! id，并且仔细阅读群公告，群文件中的比赛规程，以及给选手的建议。\n报名地址：https://www.wenjuan.com/s/BbYZvi/ ";
+//                break;
             default:
                 resp = "[CQ:at,qq=" + cqMsg.getUserId() + "]，欢迎加入本群。";
                 break;
