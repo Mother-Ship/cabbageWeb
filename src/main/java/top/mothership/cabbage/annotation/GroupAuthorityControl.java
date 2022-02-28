b029afba7291d3180f6d344b4f2940e2f6573a77
Mother Ship <quhansheng@gmail.com>
2022-02-28 20:03:54 +0800

    迭代赛群

--- src/main/java/top/mothership/cabbage/annotation/GroupAuthorityControl.java
+++ src/main/java/top/mothership/cabbage/annotation/GroupAuthorityControl.java
@@ -34,7 +34,7 @@ public @interface GroupAuthorityControl {
             532783765L,
             210342787L,
             201872650L,
-            895214831,
+            945263863,
             263668213L,
     };
 
