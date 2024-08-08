1df2f6633486bc4b17f3e82fa7d5ef1fd31fca4e
QHS <quhansheng@gmail.com>
2024-08-09 00:00:42 +0800

    适配3.19的bonus pp改动

--- src/main/java/top/mothership/cabbage/service/CqServiceImpl.java
+++ src/main/java/top/mothership/cabbage/service/CqServiceImpl.java
@@ -1300,7 +1300,7 @@ public class CqServiceImpl {
                 + "的BonusPP为：" + new DecimalFormat("#0.00").format(bonuspp)
                 + "\n计算出的ScorePP（所有成绩提供的PP）为：" + new DecimalFormat("#0.00").format(scorepp)
                 + "\n总PP为：" + new DecimalFormat("#0.00").format(userFromAPI.getPpRaw())
-                + "\n你的成绩数大于1000，已经获取了所有的Bonus PP！"
+                + "\n你的成绩数是：" + scoreCountS
                 + "\n基于https://github.com/RoanH/osu-BonusPP项目，适配了2024-03-19的最新Bonus PP改动";
         cqMsg.setMessage(resp);
         cqManager.sendMsg(cqMsg);
